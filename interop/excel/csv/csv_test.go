01884916e977cc3597160f7c014184a4eb1cee8d
tgoikawa <kosuke.oikawa@topgate.co.jp>
2019-11-12 11:07:12 +0900

    テストケース改善 refs #16

--- interop/excel/csv/csv_test.go
+++ interop/excel/csv/csv_test.go
@@ -1,6 +1,8 @@
 package csv
 
 import (
+	"encoding/csv"
+	"io"
 	"testing"
 
 	"bytes"
@@ -9,7 +11,7 @@ import (
 	"golang.org/x/text/encoding/japanese"
 )
 
-func TestNewReaderAsSJIS(t *testing.T) {
+func TestReaders(t *testing.T) {
 	var tests = []struct {
 		name     string
 		expected [][]string
@@ -59,215 +61,15 @@ func TestNewReaderAsSJIS(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			assertions := assert.New(t)
-			encoder := japanese.ShiftJIS.NewEncoder()
-			given, err := encoder.Bytes([]byte(tt.given))
-			if !assertions.NoError(err) {
-				return
-			}
-			reader := NewReader(bytes.NewReader(given))
-			if !assertions.NoError(err) {
-				return
-			}
-			actual, err := reader.ReadAll()
-			if !assertions.NoError(err) {
-				return
-			}
-			assertions.EqualValues(tt.expected, actual)
+			testReader(assertions, tt.expected, tt.given, NewSJISReader, toSJIS)
+			testReader(assertions, tt.expected, tt.given, NewReader, toSJIS)
+			testReader(assertions, tt.expected, tt.given, NewReader, toUTF8WithBOM)
+			testReader(assertions, tt.expected, tt.given, NewUTF8WithBOMReader, toUTF8WithBOM)
 		})
 	}
 }
 
-func TestNewSJISReader(t *testing.T) {
-	var tests = []struct {
-		name     string
-		expected [][]string
-		given    string
-	}{
-		{
-			"最終行改行あり",
-			[][]string{
-				{
-					"ヘッダー１",
-					"ヘッダー２",
-					"ヘッダー３",
-				},
-				{
-					"値1",
-					"値2",
-					"値3",
-				},
-			},
-			"ヘッダー１,ヘッダー２,ヘッダー３\r\n" +
-				"値1,値2,値3\r\n",
-		},
-		{
-			"最終行改行なし",
-			[][]string{
-				{
-					"ヘッダー１",
-					"ヘッダー２",
-					"ヘッダー３",
-				},
-				{
-					"値1",
-					"値2",
-					"値3",
-				},
-			},
-			"ヘッダー１,ヘッダー２,ヘッダー３\r\n" +
-				"値1,値2,値3",
-		},
-		{
-			"empty",
-			nil,
-			"",
-		},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			assertions := assert.New(t)
-			encoder := japanese.ShiftJIS.NewEncoder()
-			given, err := encoder.Bytes([]byte(tt.given))
-			if !assertions.NoError(err) {
-				return
-			}
-			reader := NewSJISReader(bytes.NewReader(given))
-			if !assertions.NoError(err) {
-				return
-			}
-			actual, err := reader.ReadAll()
-			if !assertions.NoError(err) {
-				return
-			}
-			assertions.EqualValues(tt.expected, actual)
-		})
-	}
-}
-func TestNewReaderASUTF8WithBOM(t *testing.T) {
-
-	var tests = []struct {
-		name     string
-		expected [][]string
-		given    string
-	}{
-		{
-			"最終行改行あり",
-			[][]string{
-				{
-					"ヘッダー１",
-					"ヘッダー２",
-					"ヘッダー３",
-				},
-				{
-					"値1",
-					"値2",
-					"値3",
-				},
-			},
-			"ヘッダー１,ヘッダー２,ヘッダー３\r\n" +
-				"値1,値2,値3\r\n",
-		},
-		{
-			"最終行改行なし",
-			[][]string{
-				{
-					"ヘッダー１",
-					"ヘッダー２",
-					"ヘッダー３",
-				},
-				{
-					"値1",
-					"値2",
-					"値3",
-				},
-			},
-			"ヘッダー１,ヘッダー２,ヘッダー３\r\n" +
-				"値1,値2,値3",
-		},
-		{
-			"empty",
-			nil,
-			"",
-		},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			assertions := assert.New(t)
-			given := append(UTF8BOM[:], []byte(tt.given)...)
-			reader := NewReader(bytes.NewReader(given))
-			actual, err := reader.ReadAll()
-			if !assertions.NoError(err) {
-				return
-			}
-			assertions.EqualValues(tt.expected, actual)
-		})
-	}
-}
-
-func TestNewUTF8WithBOMReader(t *testing.T) {
-
-	var tests = []struct {
-		name     string
-		expected [][]string
-		given    string
-	}{
-		{
-			"最終行改行あり",
-			[][]string{
-				{
-					"ヘッダー１",
-					"ヘッダー２",
-					"ヘッダー３",
-				},
-				{
-					"値1",
-					"値2",
-					"値3",
-				},
-			},
-			"ヘッダー１,ヘッダー２,ヘッダー３\r\n" +
-				"値1,値2,値3\r\n",
-		},
-		{
-			"最終行改行なし",
-			[][]string{
-				{
-					"ヘッダー１",
-					"ヘッダー２",
-					"ヘッダー３",
-				},
-				{
-					"値1",
-					"値2",
-					"値3",
-				},
-			},
-			"ヘッダー１,ヘッダー２,ヘッダー３\r\n" +
-				"値1,値2,値3",
-		},
-		{
-			"empty",
-			nil,
-			"",
-		},
-	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			assertions := assert.New(t)
-			given := append(UTF8BOM[:], []byte(tt.given)...)
-			reader := NewUTF8WithBOMReader(bytes.NewReader(given))
-			actual, err := reader.ReadAll()
-			if !assertions.NoError(err) {
-				return
-			}
-			assertions.EqualValues(tt.expected, actual)
-		})
-	}
-}
-func TestNewSJISWriter(t *testing.T) {
+func TestWriters(t *testing.T) {
 	var tests = []struct {
 		name     string
 		expected string
@@ -300,75 +102,56 @@ func TestNewSJISWriter(t *testing.T) {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			assertions := assert.New(t)
-			iw := bytes.Buffer{}
-			writer := NewSJISWriter(&iw)
-			err := writer.WriteAll(tt.given)
-			if !assertions.NoError(err) {
-				return
-			}
-			var expected []byte
-			if len(tt.expected) > 0 {
-				encoder := japanese.ShiftJIS.NewEncoder()
-				expected, err = encoder.Bytes([]byte(tt.expected))
-				if !assertions.NoError(err) {
-					return
-				}
-			} else {
-				expected = nil
-			}
-
-			assertions.EqualValues(expected, iw.Bytes())
+			testWriter(assertions, tt.expected, tt.given, NewUTF8WithBOMWriter, toUTF8WithBOM)
+			testWriter(assertions, tt.expected, tt.given, NewSJISWriter, toSJIS)
 		})
 	}
 }
-func TestNewUTF8WithBOMWriter(t *testing.T) {
-	var tests = []struct {
-		name     string
-		expected string
-		given    [][]string
-	}{
-		{
-			"正常系",
-			"ヘッダー１,ヘッダー２,ヘッダー３\r\n" +
-				"値1,値2,値3\r\n",
-			[][]string{
-				{
-					"ヘッダー１",
-					"ヘッダー２",
-					"ヘッダー３",
-				},
-				{
-					"値1",
-					"値2",
-					"値3",
-				},
-			},
-		},
-		{
-			"empty",
-			"",
-			nil,
-		},
+func testReader(assertions *assert.Assertions, expected [][]string, given string, generator func(io.Reader) *csv.Reader, strConverter func(string) []byte) {
+	reader := generator(bytes.NewReader(strConverter(given)))
+	actual, err := reader.ReadAll()
+	if !assertions.NoError(err) {
+		return
+	}
+	assertions.EqualValues(expected, actual)
+
+	individualReader := generator(bytes.NewReader(strConverter(given)))
+
+	index := 0
+	for line, err := individualReader.Read(); err != io.EOF; line, err = individualReader.Read() {
+		if !assertions.NoError(err) {
+			break
+		}
+		assertions.EqualValues(expected[index], line)
+		index++
 	}
 
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			assertions := assert.New(t)
-			iw := bytes.Buffer{}
-			writer := NewUTF8WithBOMWriter(&iw)
-			err := writer.WriteAll(tt.given)
-			if !assertions.NoError(err) {
-				return
-			}
-			var expected []byte
-			if len(tt.expected) > 0 {
-				expected = append(UTF8BOM[:], tt.expected...)
+}
 
-			} else {
-				expected = nil
-			}
-			assertions.EqualValues(expected, iw.Bytes())
-		})
+func testWriter(assertions *assert.Assertions, expected string, given [][]string, generator func(io.Writer) *csv.Writer, strConverter func(string) []byte) {
+	iw := &bytes.Buffer{}
+	writer := generator(iw)
+	err := writer.WriteAll(given)
+	if !assertions.NoError(err) {
+		return
 	}
+	if len(expected) > 0 {
+		expectedBytes := strConverter(expected)
+		assertions.EqualValues(expectedBytes, iw.Bytes())
+	} else {
+		assertions.Empty(iw.Bytes())
+	}
+}
+
+func toSJIS(str string) []byte {
+	encoder := japanese.ShiftJIS.NewEncoder()
+	result, err := encoder.Bytes([]byte(str))
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func toUTF8WithBOM(str string) []byte {
+	return append(UTF8BOM[:], []byte(str)...)
 }
