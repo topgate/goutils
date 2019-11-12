7ce6bf5faf39b5f456fcb9039b548d36f96bb753
tgoikawa <kosuke.oikawa@topgate.co.jp>
2019-11-12 11:22:30 +0900

    テストケースを関数名でわかるようにした refs #16

--- interop/excel/csv/csv_test.go
+++ interop/excel/csv/csv_test.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/topgate/goutils/reflect"
 	"golang.org/x/text/encoding/japanese"
 )
 
@@ -35,6 +36,23 @@ func TestReaders(t *testing.T) {
 				"値1,値2,値3\r\n",
 		},
 		{
+			"最終行改行ありCRなし",
+			[][]string{
+				{
+					"ヘッダー１",
+					"ヘッダー２",
+					"ヘッダー３",
+				},
+				{
+					"値1",
+					"値2",
+					"値3",
+				},
+			},
+			"ヘッダー１,ヘッダー２,ヘッダー３\n" +
+				"値1,値2,値3\n",
+		},
+		{
 			"最終行改行なし",
 			[][]string{
 				{
@@ -52,6 +70,23 @@ func TestReaders(t *testing.T) {
 				"値1,値2,値3",
 		},
 		{
+			"最終行改行なしCRなし",
+			[][]string{
+				{
+					"ヘッダー１",
+					"ヘッダー２",
+					"ヘッダー３",
+				},
+				{
+					"値1",
+					"値2",
+					"値3",
+				},
+			},
+			"ヘッダー１,ヘッダー２,ヘッダー３\n" +
+				"値1,値2,値3",
+		},
+		{
 			"empty",
 			nil,
 			"",
@@ -60,11 +95,10 @@ func TestReaders(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			assertions := assert.New(t)
-			testReader(assertions, tt.expected, tt.given, NewSJISReader, toSJIS)
-			testReader(assertions, tt.expected, tt.given, NewReader, toSJIS)
-			testReader(assertions, tt.expected, tt.given, NewReader, toUTF8WithBOM)
-			testReader(assertions, tt.expected, tt.given, NewUTF8WithBOMReader, toUTF8WithBOM)
+			testReader(t, tt.expected, tt.given, NewSJISReader, toSJIS)
+			testReader(t, tt.expected, tt.given, NewReader, toSJIS)
+			testReader(t, tt.expected, tt.given, NewReader, toUTF8WithBOM)
+			testReader(t, tt.expected, tt.given, NewUTF8WithBOMReader, toUTF8WithBOM)
 		})
 	}
 }
@@ -101,46 +135,54 @@ func TestWriters(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			assertions := assert.New(t)
-			testWriter(assertions, tt.expected, tt.given, NewUTF8WithBOMWriter, toUTF8WithBOM)
-			testWriter(assertions, tt.expected, tt.given, NewSJISWriter, toSJIS)
+			testWriter(t, tt.expected, tt.given, NewUTF8WithBOMWriter, toUTF8WithBOM)
+			testWriter(t, tt.expected, tt.given, NewSJISWriter, toSJIS)
 		})
 	}
 }
-func testReader(assertions *assert.Assertions, expected [][]string, given string, generator func(io.Reader) *csv.Reader, strConverter func(string) []byte) {
-	reader := generator(bytes.NewReader(strConverter(given)))
-	actual, err := reader.ReadAll()
-	if !assertions.NoError(err) {
-		return
-	}
-	assertions.EqualValues(expected, actual)
+func testReader(t *testing.T, expected [][]string, given string, generator func(io.Reader) *csv.Reader, strConverter func(string) []byte) {
+	t.Run(reflect.GetFunctionName(generator)+":"+reflect.GetFunctionName(strConverter), func(t *testing.T) {
+		assertions := assert.New(t)
+		reader := generator(bytes.NewReader(strConverter(given)))
 
-	individualReader := generator(bytes.NewReader(strConverter(given)))
-
-	index := 0
-	for line, err := individualReader.Read(); err != io.EOF; line, err = individualReader.Read() {
+		actual, err := reader.ReadAll()
 		if !assertions.NoError(err) {
-			break
+			return
 		}
-		assertions.EqualValues(expected[index], line)
-		index++
-	}
+		assertions.EqualValues(expected, actual)
+
+		individualReader := generator(bytes.NewReader(strConverter(given)))
+
+		index := 0
+		for line, err := individualReader.Read(); err != io.EOF; line, err = individualReader.Read() {
+			if !assertions.NoError(err) {
+				break
+			}
+			assertions.EqualValues(expected[index], line)
+			index++
+		}
+	})
 
 }
 
-func testWriter(assertions *assert.Assertions, expected string, given [][]string, generator func(io.Writer) *csv.Writer, strConverter func(string) []byte) {
-	iw := &bytes.Buffer{}
-	writer := generator(iw)
-	err := writer.WriteAll(given)
-	if !assertions.NoError(err) {
-		return
-	}
-	if len(expected) > 0 {
-		expectedBytes := strConverter(expected)
-		assertions.EqualValues(expectedBytes, iw.Bytes())
-	} else {
-		assertions.Empty(iw.Bytes())
-	}
+func testWriter(t *testing.T, expected string, given [][]string, generator func(io.Writer) *csv.Writer, strConverter func(string) []byte) {
+
+	t.Run(reflect.GetFunctionName(generator)+":"+reflect.GetFunctionName(strConverter), func(t *testing.T) {
+		assertions := assert.New(t)
+		iw := &bytes.Buffer{}
+		writer := generator(iw)
+		err := writer.WriteAll(given)
+		if !assertions.NoError(err) {
+			return
+		}
+		if len(expected) > 0 {
+			expectedBytes := strConverter(expected)
+			assertions.EqualValues(expectedBytes, iw.Bytes())
+		} else {
+			assertions.Empty(iw.Bytes())
+		}
+
+	})
 }
 
 func toSJIS(str string) []byte {
