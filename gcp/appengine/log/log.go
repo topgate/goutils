2305afddbfbfcb2088d76bc8c5bd6c8a67b6872f
ryutah <ryuta0221@gmail.com>
2019-11-18 19:25:30 +0900

    feat(gcp): format指定不要なログ出力関数を追加 #18

--- gcp/appengine/log/log.go
+++ gcp/appengine/log/log.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"runtime"
@@ -27,39 +28,83 @@ const (
 
 // Debugf デバッグログを出力する
 func Debugf(ctx context.Context, format string, v ...interface{}) {
-	logPrintf(ctx, severityDebug, format, v...)
+	debugf(ctx, fmt.Sprintf(format, v...))
+}
+
+// Debug デバッグログを出力する
+func Debug(ctx context.Context, v interface{}) {
+	debugf(ctx, fmt.Sprint(v))
+}
+
+func debugf(ctx context.Context, msg string) {
+	logPrintf(ctx, severityDebug, 2, msg)
 }
 
 // Infof 情報ログを出力する
 func Infof(ctx context.Context, format string, v ...interface{}) {
-	logPrintf(ctx, severityInfo, format, v...)
+	infof(ctx, fmt.Sprintf(format, v...))
+}
+
+// Info 情報ログを出力する
+func Info(ctx context.Context, v interface{}) {
+	infof(ctx, fmt.Sprint(v))
+}
+
+func infof(ctx context.Context, msg string) {
+	logPrintf(ctx, severityInfo, 2, msg)
 }
 
 // Errorf エラーログを出力する
 func Errorf(ctx context.Context, format string, v ...interface{}) {
-	logPrintf(ctx, severityError, format, v...)
+	errorf(ctx, fmt.Sprintf(format, v...))
+}
+
+// Error エラーログを出力する
+func Error(ctx context.Context, v interface{}) {
+	errorf(ctx, fmt.Sprint(v))
+}
+
+func errorf(ctx context.Context, msg string) {
+	logPrintf(ctx, severityError, 2, msg)
 }
 
 // Warningf 警告ログを出力する
 func Warningf(ctx context.Context, format string, v ...interface{}) {
-	logPrintf(ctx, severityWarn, format, v...)
+	warningf(ctx, fmt.Sprintf(format, v...))
+}
+
+// Warning 警告ログを出力する
+func Warning(ctx context.Context, v interface{}) {
+	warningf(ctx, fmt.Sprint(v))
+}
+
+func warningf(ctx context.Context, msg string) {
+	logPrintf(ctx, severityWarn, 2, msg)
 }
 
 // Criticalf 重大エラーログを出力する
 func Criticalf(ctx context.Context, format string, v ...interface{}) {
-	logPrintf(ctx, severityCritical, format, v...)
+	criticalf(ctx, fmt.Sprintf(format, v...))
 }
 
-func logPrintf(ctx context.Context, s severity, format string, v ...interface{}) {
+// Critical 重大エラーログを出力する
+func Critical(ctx context.Context, v interface{}) {
+	criticalf(ctx, fmt.Sprint(v))
+}
+
+func criticalf(ctx context.Context, msg string) {
+	logPrintf(ctx, severityCritical, 2, msg)
+}
+
+func logPrintf(ctx context.Context, s severity, skipFrame int, msg string) {
 	// プレフィックスに余計な文字列がつかないようにLoggerオブジェクトを作成
-	logger := log.New(os.Stdout, "", 0)
+	logger := log.New(getWriter(), "", 0)
 
 	// 構造化ロギングに出力する情報
 	var (
-		projectID         = appengine.ProjectID(ctx)
-		traceID           = appengine.TraceID(ctx)
-		pc, file, line, _ = runtime.Caller(2)
-		f                 = runtime.FuncForPC(pc)
+		projectID = appengine.ProjectID(ctx)
+		traceID   = appengine.TraceID(ctx)
+		info, _   = getCallerInfo(skipFrame + 1)
 	)
 	if projectID == "" || traceID == "" {
 		logger.Println("application might not be run in App Engine")
@@ -68,15 +113,38 @@ func logPrintf(ctx context.Context, s severity, format string, v ...interface{})
 	// 設定可能な特殊フィールドについては次を参照
 	//   https://cloud.google.com/logging/docs/agent/configuration?hl=ja#special-fields
 	entry := map[string]interface{}{
-		"message":                      fmt.Sprintf(format, v...),
+		"message":                      msg,
 		"severity":                     s,
 		"logging.googleapis.com/trace": fmt.Sprintf("projects/%s/traces/%s", projectID, traceID),
 		"logging.googleapis.com/sourceLocation": map[string]interface{}{
-			"file":     path.Base(file),
-			"line":     line,
-			"function": f.Name(),
+			"file":     info.File,
+			"line":     info.Line,
+			"function": info.FnName,
 		},
 	}
 	payload, _ := json.Marshal(entry)
 	logger.Println(string(payload))
+}
+
+var getWriter = func() io.Writer {
+	return os.Stdout
+}
+
+type callerInfo struct {
+	File   string
+	Line   int
+	FnName string
+}
+
+var getCallerInfo = func(skipFrame int) (callerInfo, bool) {
+	pc, file, line, ok := runtime.Caller(skipFrame + 1)
+	if !ok {
+		return callerInfo{}, ok
+	}
+	f := runtime.FuncForPC(pc)
+	return callerInfo{
+		File:   path.Base(file),
+		Line:   line,
+		FnName: f.Name(),
+	}, true
 }
