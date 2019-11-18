865bdb0bbe7475e1a13090113f83d400543fc0fd
ryutah <ryuta0221@gmail.com>
2019-11-18 19:26:46 +0900

    chore(gcp): ログ出力テストケースを追加 #18

--- gcp/appengine/log/log_test.go
+++ gcp/appengine/log/log_test.go
@@ -1,33 +1,180 @@
-package log
+package log_test
 
 import (
-	"net/http"
-	"os"
+	"bytes"
+	"context"
+	"io"
+	"testing"
 
+	"fmt"
+
+	"github.com/stretchr/testify/assert"
 	"github.com/topgate/goutils/gcp/appengine"
+	. "github.com/topgate/goutils/gcp/appengine/log"
 )
 
-func Example() {
-	r := setupEnvAndNewRequest("sample_project_id", "sample_trace_id/xxxxxxxxxx")
-	ctx := appengine.NewContext(r)
+func TestPrintf(t *testing.T) {
+	info := CallerInfo{
+		File:   "log_test.go",
+		FnName: "github.com/topgate/goutils/gcp/appengine/log.TestPrintf",
+		Line:   10,
+	}
+	const (
+		projectID = "test_project"
+		traceID   = "trace_id"
+	)
 
-	Debugf(ctx, "debug %v", "foo")
-	Infof(ctx, "info %v", "foo")
-	Warningf(ctx, "warning %v", "foo")
-	Errorf(ctx, "error %v", "foo")
-	Criticalf(ctx, "critical %v", "foo")
+	type (
+		in struct {
+			format string
+			v      []interface{}
+		}
+	)
+	cases := []struct {
+		name     string
+		fn       func(ctx context.Context, format string, v ...interface{})
+		in       in
+		expected string
+	}{
+		{
+			name: "Debugf",
+			fn:   Debugf,
+			in: in{
+				format: "test %v",
+				v:      []interface{}{"debug"},
+			},
+			expected: expectLogMessage(SeverityDebug, projectID, traceID, info.File, info.FnName, info.Line, fmt.Sprintf("test %v", "debug")),
+		},
+		{
+			name: "Infof",
+			fn:   Infof,
+			in: in{
+				format: "test2 %v",
+				v:      []interface{}{"info"},
+			},
+			expected: expectLogMessage(SeverityInfo, projectID, traceID, info.File, info.FnName, info.Line, fmt.Sprintf("test2 %v", "info")),
+		},
+		{
+			name: "Warningf",
+			fn:   Warningf,
+			in: in{
+				format: "test3 %v",
+				v:      []interface{}{"warning"},
+			},
+			expected: expectLogMessage(SeverityWarn, projectID, traceID, info.File, info.FnName, info.Line, fmt.Sprintf("test3 %v", "warning")),
+		},
+		{
+			name: "Errorf",
+			fn:   Errorf,
+			in: in{
+				format: "test4 %v",
+				v:      []interface{}{"error"},
+			},
+			expected: expectLogMessage(SeverityError, projectID, traceID, info.File, info.FnName, info.Line, fmt.Sprintf("test4 %v", "error")),
+		},
+		{
+			name: "Criticalf",
+			fn:   Criticalf,
+			in: in{
+				format: "test5 %v",
+				v:      []interface{}{"critical"},
+			},
+			expected: expectLogMessage(SeverityCritical, projectID, traceID, info.File, info.FnName, info.Line, fmt.Sprintf("test5 %v", "critical")),
+		},
+	}
 
-	// Output:
-	// {"logging.googleapis.com/sourceLocation":{"file":"log_test.go","function":"github.com/topgate/goutils/gcp/appengine/log.Example","line":14},"logging.googleapis.com/trace":"projects/sample_project_id/traces/sample_trace_id","message":"debug foo","severity":"DEBUG"}
-	// {"logging.googleapis.com/sourceLocation":{"file":"log_test.go","function":"github.com/topgate/goutils/gcp/appengine/log.Example","line":15},"logging.googleapis.com/trace":"projects/sample_project_id/traces/sample_trace_id","message":"info foo","severity":"INFO"}
-	// {"logging.googleapis.com/sourceLocation":{"file":"log_test.go","function":"github.com/topgate/goutils/gcp/appengine/log.Example","line":16},"logging.googleapis.com/trace":"projects/sample_project_id/traces/sample_trace_id","message":"warning foo","severity":"WARNING"}
-	// {"logging.googleapis.com/sourceLocation":{"file":"log_test.go","function":"github.com/topgate/goutils/gcp/appengine/log.Example","line":17},"logging.googleapis.com/trace":"projects/sample_project_id/traces/sample_trace_id","message":"error foo","severity":"ERROR"}
-	// {"logging.googleapis.com/sourceLocation":{"file":"log_test.go","function":"github.com/topgate/goutils/gcp/appengine/log.Example","line":18},"logging.googleapis.com/trace":"projects/sample_project_id/traces/sample_trace_id","message":"critical foo","severity":"CRITICAL"}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var w bytes.Buffer
+
+			wFinish := SetWriterGetter(func() io.Writer { return &w })
+			cFinish := SetCallerInfoGetter(func(skipFrame int) (CallerInfo, bool) { return info, true })
+			defer func() {
+				wFinish()
+				cFinish()
+			}()
+
+			assertions := assert.New(t)
+			ctx := appengine.NewContext(setupEnvAndNewRequest(projectID, traceID))
+			c.fn(ctx, c.in.format, c.in.v...)
+			assertions.Equal(c.expected, w.String())
+		})
+	}
 }
 
-func setupEnvAndNewRequest(projectID, traceID string) *http.Request {
-	os.Setenv("GOOGLE_CLOUD_PROJECT", projectID)
-	req, _ := http.NewRequest(http.MethodGet, "", nil)
-	req.Header.Set("X-Cloud-Trace-Context", traceID)
-	return req
+func TestPrint(t *testing.T) {
+	info := CallerInfo{
+		File:   "log_test.go",
+		FnName: "github.com/topgate/goutils/gcp/appengine/log.TestPrint",
+		Line:   10,
+	}
+	const (
+		projectID = "test_project"
+		traceID   = "trace_id"
+	)
+
+	cases := []struct {
+		name     string
+		fn       func(ctx context.Context, v interface{})
+		in       interface{}
+		expected string
+	}{
+		{
+			name:     "Debug",
+			fn:       Debug,
+			in:       "test",
+			expected: expectLogMessage(SeverityDebug, projectID, traceID, info.File, info.FnName, info.Line, "test"),
+		},
+		{
+			name:     "Info",
+			fn:       Info,
+			in:       "test2",
+			expected: expectLogMessage(SeverityInfo, projectID, traceID, info.File, info.FnName, info.Line, "test2"),
+		},
+		{
+			name:     "Warning",
+			fn:       Warning,
+			in:       "test3",
+			expected: expectLogMessage(SeverityWarn, projectID, traceID, info.File, info.FnName, info.Line, "test3"),
+		},
+		{
+			name:     "Error",
+			fn:       Error,
+			in:       "test4",
+			expected: expectLogMessage(SeverityError, projectID, traceID, info.File, info.FnName, info.Line, "test4"),
+		},
+		{
+			name:     "Critical",
+			fn:       Critical,
+			in:       "test5",
+			expected: expectLogMessage(SeverityCritical, projectID, traceID, info.File, info.FnName, info.Line, "test5"),
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var w bytes.Buffer
+
+			wFinish := SetWriterGetter(func() io.Writer { return &w })
+			cFinish := SetCallerInfoGetter(func(skipFrame int) (CallerInfo, bool) { return info, true })
+			defer func() {
+				wFinish()
+				cFinish()
+			}()
+
+			assertions := assert.New(t)
+			ctx := appengine.NewContext(setupEnvAndNewRequest(projectID, traceID))
+			c.fn(ctx, c.in)
+			assertions.Equal(c.expected, w.String())
+		})
+	}
+}
+
+func expectLogMessage(s Severify, projectID, traceID, fileName, funcName string, line int, msg string) string {
+	return fmt.Sprintln(
+		fmt.Sprintf(
+			`{"logging.googleapis.com/sourceLocation":{"file":"%s","function":"%s","line":%d},"logging.googleapis.com/trace":"projects/%s/traces/%s","message":"%s","severity":"%s"}`,
+			fileName, funcName, line, projectID, traceID, msg, s,
+		),
+	)
 }
