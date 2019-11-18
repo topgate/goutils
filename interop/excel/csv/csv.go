18918e53b9b933a1b0ce461d8d846774a24b7cef
tgoikawa <kosuke.oikawa@topgate.co.jp>
2019-11-18 19:17:23 +0900

    関数の説明に移動

--- interop/excel/csv/csv.go
+++ interop/excel/csv/csv.go
@@ -37,12 +37,14 @@ func NewReader(r io.Reader) *csv.Reader {
 	})
 }
 
-// NewSJISWriter 与えられたio.Writerを元に新しいSJISのcsvライターを返す。UseCRLFはデフォルトでtrueが設定される
+// NewSJISWriter 与えられたio.Writerを元に新しいSJISのcsvライターを返す。
+//
+// UseCRLFはデフォルトでtrueが設定される
+//
+// panicが発生するため*bufio.Writerをwに与えてはならない。
+// *bufio.Writerを与えるとpanicになるのは、返されるWriterのFlushを呼び出しても、与えた*bufio.WriterのFlushが呼び出されないため。
+// これはencoding/csvのWriterの設計に依存する制約である。
 func NewSJISWriter(w io.Writer) *csv.Writer {
-
-	// BUG(tgoikawa): panicが発生するため*bufio.Writerをwに与えてはならない
-	// *bufio.Writerを与えるとpanicになるのは、返されるWriterのFlushを呼び出しても、与えた*bufio.WriterのFlushが呼び出されないため
-	// これはencoding/csvのWriterの設計に依存する制約である
 	if _, ok := w.(*bufio.Writer); ok {
 		panic("Can't use *bufio.Writer")
 	}
@@ -51,12 +53,14 @@ func NewSJISWriter(w io.Writer) *csv.Writer {
 	return writer
 }
 
-// NewUTF8WithBOMWriter 与えられたio.Writerを元に新しいBOM付きUTF8のcsvライターを返す。UseCRLFはデフォルトでtrueが設定される
+// NewUTF8WithBOMWriter 与えられたio.Writerを元に新しいBOM付きUTF8のcsvライターを返す。
+//
+// UseCRLFはデフォルトでtrueが設定される。
+//
+// panicが発生するため*bufio.Writerをwに与えてはならない。
+// *bufio.Writerを与えるとpanicになるのは、返されるWriterのFlushを呼び出しても、与えた*bufio.WriterのFlushが呼び出されないため。
+// これはencoding/csvのWriterの設計に依存する制約である。
 func NewUTF8WithBOMWriter(w io.Writer) *csv.Writer {
-
-	// BUG(tgoikawa): panicが発生するため*bufio.Writerをwに与えてはならない
-	// *bufio.Writerを与えるとpanicになるのは、返されるWriterのFlushを呼び出しても、与えた*bufio.WriterのFlushが呼び出されないため
-	// これはencoding/csvのWriterの設計に依存する制約である
 	if _, ok := w.(*bufio.Writer); ok {
 		panic("Can't use *bufio.Writer")
 	}
